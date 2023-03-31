31627ca9a8ddd5e40b3343f8531c1cd8feef091b
thinksecretly <thinkjenness@gmail.com>
2023-03-31 14:32:39 -0400

    added encryption

--- pwd_manager.py
+++ pwd_manager.py
@@ -1,16 +1,29 @@
 from cryptography.fernet import Fernet
 # get input for master password
 
-pwd = input("Enter master password: ")
-
-if pwd == "password":
-    print("Welcome to the password manager")
-
-'''def write_key():
+'''
+def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
-write_key()'''
+write_key()
+'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_pwd = input("Enter master password: ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+if master_pwd == "password":
+    print("Welcome to the password manager")
+
+
 
 def add():
     pass
@@ -18,14 +31,14 @@ def add():
     username = input("Enter the username: ")
     password = input("Enter the password: ")
     with open ("passwords.txt", "a") as f:
-        f.write(nameofservice + " | " + username + " | " + password + "" + '\n' )
+        f.write(nameofservice + " | " + username + " | " + str(fer.encrypt(password.encode()).decode) + "" + '\n' )
 
 def view():
     with open ("passwords.txt", "r") as f:
         for line in f.readlines():
             data = print(line.rstrip())
             service, usr, pwd = data.split(" | ")
-            print("Service: " + service + " , Username: " + usr + " , Password: " + pwd)
+            print("Service: " + service + " , Username: " + usr + " , Password: " + fer.decrypt(pwd.encode()).decode)
 
 while True:
     choice = input("Would you like to add a new password or view an existing one? Or enter exit to quit (add/view/exit)")
