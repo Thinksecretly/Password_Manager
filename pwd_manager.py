4cfc6a4196f0f85cea3a04a4b3d8f583538d498c
thinksecretly <thinkjenness@gmail.com>
2023-03-31 17:16:03 -0400

    fixed errors  in  splits

--- pwd_manager.py
+++ pwd_manager.py
@@ -30,19 +30,18 @@ else:
 
 
 def add():
-    pass
     nameofservice = input("Enter the name of the website: ")
     username = input("Enter the username: ")
     password = input("Enter the password: ")
     with open ("passwords.txt", "a") as f:
-        f.write(nameofservice + " | " + username + " | " + str(fer.encrypt(password.encode()).decode) + "" + '\n' )
+        f.write(nameofservice + "|" + username + "|" + str(fer.encrypt(password.encode()).decode()) + "\n")
 
 def view():
     with open ("passwords.txt", "r") as f:
         for line in f.readlines():
-            data = print(line.rstrip())
-            service, usr, pwd = data.split(" | ")
-            print("Service: " + service + " , Username: " + usr + " , Password: " + fer.decrypt(pwd.encode()).decode)
+            data = line.rstrip()
+            service, usr, pwd = data.split("|")
+            print("Service: " + service + " , Username: " + usr + " , Password: " + fer.decrypt(pwd.encode()).decode())
 
 while True:
     choice = input("Would you like to add a new password or view an existing one? Or enter exit to quit (add/view/exit)")
